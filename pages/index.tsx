b020453c56c4ba515bd40b4ee0923327a7dfc8e1
Manasgupta840 <manasgupta@Mac.lan>
2025-09-30 23:00:19 +0530

    fix: change

--- pages/index.tsx
+++ pages/index.tsx
@@ -502,8 +502,6 @@ export default function HealthCheckApp() {
 
       // If the server did NOT stream, fall back to JSON once.
       if (!response.body) {
-        // const result = await response.json();
-        // handleFinalResult(result, streamMsgId);
         return;
       }
 
